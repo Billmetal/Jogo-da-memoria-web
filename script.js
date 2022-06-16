268a7639ac5465fba0f6a82f27e4a87ae301e2b4
Billmetal <billmetal9@gmail.com>
2022-06-16 19:52:14 -0300

    Implementação da comparação de cartas.

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const cards = document.querySelectorAll(".card");
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() { 
-    this.classList.toggle("flip");
+    this.classList.add("flip");
+    if (!hasFlippedCard) { 
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+function disableCards() { 
+
+}
+
+function checkForMatch() { 
+    if (firstCard.dataset.card === secondCard.dataset.card) { 
+        disableCards();
+        return;
+    }
+    unflipCards();
 }
 
 cards.forEach((card) => { 
